4a17e4e8d832afc2416e91fd0573fe073a6958bd
Jeremy Adams <jeremy@dagger.io>
2024-05-02 07:05:06 -0700

    break into multiple functions
    Signed-off-by: Jeremy Adams <jeremy@dagger.io>

--- src/main/__init__.py
+++ src/main/__init__.py
@@ -9,8 +9,8 @@ from dagger import dag, function, object_type
 class Milan:
     
     @function
-    async def test(self, source: dagger.Directory) -> dagger.Container:
-        """Build a Container from a Python project"""
+    async def poetry_base(self) -> dagger.Container:
+        """Build an Ubuntu Container with Poetry"""
         return await (
             dag.container()
             .from_("ubuntu:latest")
@@ -20,8 +20,14 @@ class Milan:
             .with_exec(["apt", "install", "python3", "python-is-python3", "python3-venv", "-y"])
             .with_exec(["bash", "-c", "curl -sSL https://install.python-poetry.org | POETRY_HOME=/opt/poetry python3 -"])
             .with_exec(["bash", "-c", "ln -T /opt/poetry/bin/poetry /usr/local/bin/poetry"])
+        )
+
+    @function
+    async def test(self, source: dagger.Directory) -> dagger.Container:
+        """Build an Ubuntu Container with Poetry"""
+        return await (
+            self.poetry_base()
             .with_directory("/src", source)
             .with_workdir("/src")
             .with_exec(["poetry", "install"])
-			.sync()
         )
